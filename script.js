31a9e185e28ab636af8dd860c0570e0482fbe97e
anilpunnapureddy <72924916+anilpunnapureddy@users.noreply.github.com>
2023-12-06 00:28:31 +0530

    added footer

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // JavaScript to add a class when scrolling down and remove it when scrolling back to the top
 window.addEventListener('scroll', function() {
-    const header = document.querySelector('.sticky-header');
-    const scrollPosition = window.scrollY;
-  
-    if (scrollPosition > 50) {
-      header.classList.add('sticky');
-    } else {
-      header.classList.remove('sticky');
-    }
-  });
+  const header = document.querySelector('.sticky-header');
+  const content = document.querySelector('.content');
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 50) {
+    header.classList.add('sticky');
+    content.classList.add('sticky-content');
+  } else {
+    header.classList.remove('sticky');
+    content.classList.remove('sticky-content');
+  }
+});
+
   
